2938e351e3ded815ad1c5db184882d3c5b70dca3
wabosc <eyouth00700@gmail.com>
2022-08-15 09:40:08 +0800

    Update

---
+++ update/admin/home.php
@@ -0,0 +1,27 @@
+</style>
+
+<div class="containe-fluid">
+
+	<div class="row">
+		<div class="col-lg-12">
+			
+		</div>
+	</div>
+
+	<div class="row mt-3 ml-3 mr-3">
+			<div class="col-lg-12">
+			<div class="card">
+				<div class="card-body">
+				<?php echo "Welcome back ".$_SESSION['login_name']."!"  ?>	
+				</div>
+			</div><div class="cover" align="center">
+  				<img src="../assets/img/Foodie Food Logo.png" alt="">
+  			</div>
+		</div>
+		</div>
+	</div>
+
+</div>
+<script>
+	
+</script>
